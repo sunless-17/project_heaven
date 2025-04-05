897cb08fa8495682a02f326ce97f631e3b0e235d
Sunless <sunless17@tutamail.com>
2025-04-05 23:07:42 +0300

    todo: solve "twelve days of christmas"

---
+++ rizzmas/notes.md
@@ -0,0 +1,9 @@
+# rizzmas charol song
+- utilize loops to print the entire song
+# steps
+- create new vec to store the lyrics
+- loop thru number of days (replace days index with numbers)
+- for loop with decreasing range
+## refactor
+- specify the days
+- if statement to change 1st and 2nd
---
+++ rizzmas/src/main.rs
@@ -0,0 +1,116 @@
+fn main() {
+    // On the first day of Christmas
+    // My true love sent to me
+    // A partridge in a pear tree
+
+    // On the second day of Christmas
+    // My true love sent to me
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the third day of Christmas
+    // My true love sent to me
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the fourth day of Christmas
+    // My true love sent to me
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the fifth day of Christmas
+    // My true love sent to me
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the sixth day of Christmas
+    // My true love sent to me
+    // Six geese a laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the seventh day of Christmas
+    // My true love sent to me
+    // Seven swans a swimming
+    // Six geese a-laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the eighth day of Christmas
+    // My true love sent to me
+    // Eight maids a milking
+    // Seven swans a swimming
+    // Six geese a-laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the ninth day of Christmas
+    // My true love sent to me
+    // Nine ladies dancing
+    // Eight maids a-milking
+    // Seven swans a-swimming
+    // Six geese a-laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the tenth day of Christmas
+    // My true love sent to me
+    // Ten lords a-leaping
+    // Nine ladies dancing
+    // Eight maids a-milking
+    // Seven swans a-swimming
+    // Six geese a-laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the 11th day of Christmas
+    // My true love sent to me
+    // I sent 11 pipers piping
+    // Ten lords a-leaping
+    // Nine ladies dancing
+    // Eight maids a-milking
+    // Seven swans a-swimming
+    // Six geese a-laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+
+    // On the 12th day of Christmas
+    // My true love sent to me
+    // 12 drummers drumming
+    // Eleven pipers piping
+    // Ten lords a-leaping
+    // Nine ladies dancing
+    // Eight maids a-milking
+    // Seven swans a-swimming
+    // Six geese a-laying
+    // Five golden rings (five golden rings)
+    // Four calling birds
+    // Three French hens
+    // Two turtle-doves
+    // And a partridge in a pear tree
+    // And a partridge in a pear tree
+}
