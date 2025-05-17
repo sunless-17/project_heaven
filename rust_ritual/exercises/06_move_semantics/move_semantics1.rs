a14f279662c724276c436c465388fe0a68a3079d
Sunless <sunless17@tutamail.com>
2025-05-17 19:52:18 +0300

    feat: partial move semantics

--- rust_ritual/exercises/06_move_semantics/move_semantics1.rs
+++ rust_ritual/exercises/06_move_semantics/move_semantics1.rs
@@ -1,24 +1,24 @@
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+  let mut vec = vec;
 
-    vec.push(88);
+  vec.push(88);
 
-    vec
+  vec
 }
 
 fn main() {
-    // You can optionally experiment here.
+  // You can optionally experiment here.
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn move_semantics1() {
-        let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0);
-        assert_eq!(vec1, vec![22, 44, 66, 88]);
-    }
+  #[test]
+  fn move_semantics1() {
+    let vec0 = vec![22, 44, 66];
+    let vec1 = fill_vec(vec0);
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+  }
 }
